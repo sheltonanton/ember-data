da644785fdf53b717c17da21d3e4961c6ab86a95
Infant Antony Shelton <sheltonanton@gmail.com>
2019-07-16 13:22:46 +0530

    token commit

--- app/components/demo-component.js
+++ app/components/demo-component.js
@@ -2,25 +2,40 @@ import Component from '@ember/component';
 import SearchBar from '../configurations/ui-components';
 
 export default Component.extend({
+  init: function(){
+    this._super(...arguments);
+    document.addEventListener('click',() => {
+      this.set('toggleOpenClass', '');
+    });
+  },
+  click: function(){
+    this.set('toggleOpenClass', 'search-bar--open');
+    return false;
+  },
   didInsertElement: function () {
     let properties = {
       dataSource: [{
+          category: "names",
           label: "Infant Antony Shelton",
           value: "shelton"
         },
         {
+          category: "names",
           label: "Infant Antony Sheron",
           value: "sheron"
         },
         {
+          category: "names",
           label: "Faizul Nafees",
           value: "faizul"
         },
         {
+          category: "names",
           label: "Kathiravan Kumar",
           value: "kathiravanKumar"
         },
         {
+          category: "names",
           label: "Arun Kumar",
           value: "arunKumar"
         },
--- app/configurations/ui-components.js
+++ app/configurations/ui-components.js
@@ -5,10 +5,12 @@ export default (function () {
   var input = null;
   var menu = null;
   var container = null;
-  return {
+  var token = null;
+  var returnObject = {
     createTokenField: function (element, prop) {
       container = document.createElement('div');
       container.style.position = "relative"; //setting position as relative for absolute menu to appear inside it
+      element.innerHTML = '';
       element.appendChild(container);
       let {
         dataSource: data,
@@ -33,7 +35,7 @@ export default (function () {
           })
           //populating with values
           val.forEach((v, index) => {
-            let token = this.createToken(v.label, v.value, index, options.tokens);
+            let token = this.createToken(v.label, v.value, v.category, index, options.tokens);
             container.appendChild(token); //appending the token
             container.setAttribute('data-lastTokenId', index);
           })
@@ -47,8 +49,9 @@ export default (function () {
         throw "Specify options as an Object"
       }
     },
-    createToken: function (label, data, index, property) {
-      let element = document.createElement('div');
+    createToken: function (label, data, category, index, property) {
+      let element = token || document.createElement('div');
+      element.innerHTML = '';
       element.setAttribute('id', `search_token_${index}`);
       element.classList.add('custom-search-bar--token');
       if (property && property.class) {
@@ -56,17 +59,35 @@ export default (function () {
       }
       element.setAttribute('data-id', index);
       element.setAttribute('data-value', data);
-
-      let category = document.createElement('span');
-      category.setAttribute('class', 'custom-search-bar--token__category');
-      category.innerHTML = "cat";
-      element.appendChild(category);
+      element.setAttribute('data-category', category);
+      if(category){
+        let cat = document.createElement('span');
+        cat.setAttribute('class', 'custom-search-bar--token__category');
+        cat.innerHTML = category;
+        element.appendChild(cat);
+      }
 
       let input = document.createElement('span');
       input.setAttribute('class', 'custom-search-bar--token__input');
       input.setAttribute('contenteditable', true);
+      input.addEventListener("input", (event)=>{
+        // token = element;
+        // this.triggerSearch(event.target.innerHTML, category);
+      })
       input.innerHTML = label;
       element.appendChild(input);
+
+      let close = document.createElement('span');
+      close.setAttribute('class', 'custom-search-bar--token__close');
+      element.appendChild(close);
+
+      element.addEventListener("click",(event) => {
+        if (event.target == close) {
+          setTimeout(()=>{
+            this.removeToken(element);
+          },0);
+        }
+      });
       return element;
     },
     createInput: function () {
@@ -77,9 +98,9 @@ export default (function () {
           this.popToken(container);
         }
       }
-      element.oninput = (event) => {
+      element.oninput = element.onfocus = (event)  => {
         setTimeout(() => {
-          input.value = event.target.value;
+          input.value = (event && event.target.value) || "";
           this.triggerSearch(input.value);
         }, 0);
       }
@@ -93,11 +114,11 @@ export default (function () {
     openMenu: function (filteredDataSource) {
       let list = null;
       menu.style.position = "absolute";
-      menu.style.top = container.offsetHeight + 8;
       menu.style.width = container.offsetWidth;
       menu.setAttribute('class', 'custom-search-bar--menu');
       if (filteredDataSource.length) {
         list = document.createElement("ul");
+        list.setAttribute('class', 'custom-search-bar--menu-items');
         filteredDataSource.forEach(data => {
           let item = this.createMenuItem(data);
           list.appendChild(item);
@@ -109,18 +130,28 @@ export default (function () {
       menu.innerHTML = '';
       menu.appendChild(list);
     },
+    closeMenu: function(){
+      menu.setAttribute('class', 'custom-search-bar--menu hide');
+    },
     createMenuItem: function (menuItem) {
       let element = document.createElement('li');
       element.setAttribute('class', 'custom-search-bar--menu-item');
       element.setAttribute('data-label', menuItem.label);
       element.setAttribute('data-value', menuItem.value);
+      element.setAttribute('data-category', menuItem.category);
       element.innerHTML = menuItem.label;
       element.id = menuItem.value;
       //TODO on select update the add Token method with the selected one
-      element.onclick = (event) => {
-        this.addToken(menuItem.label, menuItem.value);
-        event.stopPropagation();
-      }
+      element.addEventListener("click",(event) => {
+        if(token!= null){
+          //TODO
+          // token = this.createToken(menuItem.label, menuItem.value, menuItem.category, token.dataset.id, properties.tokens);
+          // token = null;
+        }else{
+          this.addToken(menuItem.label, menuItem.value, menuItem.category);
+          event.stopPropagation();
+        }
+      });
       return element;
     },
     showEmptyMessage: function () {
@@ -128,30 +159,39 @@ export default (function () {
       emptyContent.innerHTML = "No results found";
       return emptyContent;
     },
-    addToken: function (label, value) {
+    addToken: function (label, value, category) {
       let index = container.dataset.lastTokenId;
-      let token = this.createToken(label, value, index + 1, properties.tokens);
+      let token = this.createToken(label, value, category, index + 1, properties.tokens);
       container.insertBefore(token, input);
       container.setAttribute('data-lastTokenId', index + 1);
       //clear and focus input
       input.value = "";
-      this.triggerSearch("");
       input.focus();
     },
     popToken: function () {
       let childs = container.querySelectorAll(`[id*='search_token_']`);
       childs.length && childs[childs.length - 1].remove();
-      input.value = "";
       this.triggerSearch("");
+    },
+    removeToken: function (token) {
+      token.remove();
       input.focus();
     },
-    triggerSearch: function (searchString) {
+    startSearch: function(){
+
+    },
+    triggerSearch: function (searchString, category) {
       //TODO
       //TODO also filter the data with same category and input already present in dom
       if (Array.isArray(dataSource)) {
         let filteredDataSource = dataSource.filter(data => {
           return (data.label && data.label.includes(searchString))
         })
+        if(category!=null && category != ""){
+          filteredDataSource = filteredDataSource.filter(data => {
+            return (data.category == category);
+          })
+        }
         //check for duplicate entries
         let tokens = Array.from(container.children).map(child => {
           return child.dataset.value
@@ -178,13 +218,21 @@ export default (function () {
           url: url + (query) ? `?${query}` : '',
           method,
           success: (data) => {
-            console.log(...arguments);
+
           },
           error: (error) => {
-            console.log(...arguments);
+
           }
         });
       }
     }
   }
+  document.addEventListener("click", function (event) {
+    if(event.path.includes(menu) || event.path.includes(container)){
+      event.preventDefault();
+    }else{
+      returnObject.closeMenu();
+    }
+  });
+  return returnObject;
 }())
